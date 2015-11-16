d54f5ef93d847ec5108d80252060ec72aa52c4f5
jax9000 <jax1994@gmail.com>
2015-11-16 20:56:05 +0100

    getting string from img added (ocr lib)

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 import os
+import pytesseract
+from PIL import Image
+
 
 Xsize, Ysize = 510*2, 300*2
 nameX1, nameX2 = 260, 300
@@ -16,7 +19,7 @@ signY1, signY2 = 380, 750
 files = 16
 template = './template.jpg'
 #zdjecia powinny byc w notacji i.jpg dla i = 1, 2, 3...
-dirPath = './images/'           #input
+dirPath = './images1/'           #input
 saveDirPath = './TESTS/' + dirPath  #output
 
 def getName(img):
@@ -36,7 +39,6 @@ def transform2default(img, dst):
     pts2 = np.float32([[0,0],[0,Ysize],[Xsize,Ysize],[Xsize,0]])
     M = cv2.getPerspectiveTransform(pts1,pts2)
     return cv2.warpPerspective(img,M,(Xsize,Ysize))
-
 
              #(template, searchableImg)
 def findObject(img1,img2):
@@ -91,7 +93,7 @@ def main():
     # if not os.path.exists(dirPath):
     #     print 'input directory not exist'
     #     return 1
-    
+
     # for i in range(files):
     #     path = dirPath + str(i+1) + '.jpg'
     #
@@ -103,14 +105,19 @@ def main():
     #     cv2.imwrite(path, img3)
     #     print 'progress: ' + str((i+1)/(files*1.0) * 100) + '%' + '  file: ' + str(i+1)
 
-    path = './8.jpg'
+    img1 = cv2.imread(template)
+
+    path = dirPath + '4.jpg'
     img2 = cv2.imread(path)
+    img3 = findObject(img1, img2)
 
-    cv2.imwrite('./face.jpg', getFace(img2))
-    cv2.imwrite('./name.jpg', getName(img2))
-    cv2.imwrite('./subname.jpg', getSubName(img2))
-    cv2.imwrite('./sign.jpg', getSign(img2))
+    cv2.imwrite('./face.jpg', getFace(img3))
+    cv2.imwrite('./name.jpg', getName(img3))
+    cv2.imwrite('./subname.jpg', getSubName(img3))
+    cv2.imwrite('./sign.jpg', getSign(img3))
 
+    print(pytesseract.image_to_string(Image.open('name.jpg')))
+    print(pytesseract.image_to_string(Image.open('subname.jpg')))
     print 'done'
 
 
