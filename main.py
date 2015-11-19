18c48ad5a2cdb7056d3ec3e853bca2e0e7b305f7
jax9000 <jax1994@gmail.com>
2015-11-19 14:19:23 +0100

    binarization improvment: histogram equalization added

--- main.py
+++ main.py
@@ -36,7 +36,13 @@ def getSign(img):
 
 def img2str(img):
     cv2.imwrite('temp.jpg', img)
-    return pytesseract.image_to_string(Image.open('temp.jpg'))
+    return pytesseract.image_to_string(Image.open('temp.jpg'),lang='pol')
+
+def transform2default(img, dst):
+    pts1 = np.float32(dst)
+    pts2 = np.float32([[0,0],[0,Ysize],[Xsize,Ysize],[Xsize,0]])
+    M = cv2.getPerspectiveTransform(pts1,pts2)
+    return cv2.warpPerspective(img,M,(Xsize,Ysize))
 
 def connectVerticaly(imgUP, imgDOWN):
     h1, w1 = imgUP.shape[:2]
@@ -49,11 +55,34 @@ def connectVerticaly(imgUP, imgDOWN):
     vis[h1:h2+h1, :w2] = imgDOWN
     return vis
 
-def transform2default(img, dst):
-    pts1 = np.float32(dst)
-    pts2 = np.float32([[0,0],[0,Ysize],[Xsize,Ysize],[Xsize,0]])
-    M = cv2.getPerspectiveTransform(pts1,pts2)
-    return cv2.warpPerspective(img,M,(Xsize,Ysize))
+def binarization(img):
+    if len(img.shape) == 3:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    elif len(img.shape) != 2:
+        print 'wrong input pic binarization()'
+        return None
+
+    h, w = img.shape
+    white, black = 255, 0
+
+
+    # ret3,img = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    for i in range(0, h):
+        for j in range(0, w):
+            if img.item(i,j) <= 25: #na podstawie histogramu powinien znajdowac ta wartosc
+                val = black
+            else:
+                val = white
+            img.itemset((i,j),val) # img[i][j] = val #
+    kernel = np.ones((4,4),np.uint8)
+    img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+
+    return img
+
+
+
+
+
 
              #(template, searchableImg)
 def findObject(img1,img2):
@@ -101,14 +130,6 @@ def findObject(img1,img2):
 
 
 def main():
-
-    # img1 = cv2.imread(template)
-    # if not os.path.exists(saveDirPath):
-    #     os.makedirs(saveDirPath)
-    # if not os.path.exists(dirPath):
-    #     print 'input directory not exist'
-    #     return 1
-
     # for i in range(files):
     #     path = dirPath + str(i+1) + '.jpg'
     #
@@ -120,22 +141,60 @@ def main():
     #     cv2.imwrite(path, img3)
     #     print 'progress: ' + str((i+1)/(files*1.0) * 100) + '%' + '  file: ' + str(i+1)
 
-    fileName = '12.jpg'
+    fileName = '13.jpg'
+    dirPath = './TESTS/images/'
+    img1 = cv2.imread(template)
 
-    img1 = cv2.imread(template,0)
     path = dirPath + fileName
-    img2 = cv2.imread(path,0)
-    img3 = findObject(img1, img2)
+    img2 = cv2.imread(path)
+    cv2.imwrite('./subname.jpg', getSubName(img2))
 
-    print img2str(getName(img3)).upper()
-    print img2str(getSubName(img3)).upper()
+    img1 = cv2.imread(template)
+    if not os.path.exists(saveDirPath):
+        os.makedirs(saveDirPath)
+    if not os.path.exists(dirPath):
+        print 'input directory not exist'
+        return 1
 
-    imgUp = connectVerticaly(getFace(img3), getSign(img3))
-    imgDown = connectVerticaly(getName(img3), getSubName(img3))
+    if len(img2.shape) == 3:
+        for i in range(0, img2.shape[0]):
+            for j in range(0, img2.shape[1]):
+                img2.itemset((i,j,1),255)
+        img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+    img2 = cv2.medianBlur(img2,3)
+    img2 = cv2.equalizeHist(img2)
+
+    hist,bins = np.histogram(img2.flatten(),256,[0,256])
+
+    cdf = hist.cumsum()
+    cdf_normalized = cdf * hist.max()/ cdf.max()
+
+    plt.plot(cdf_normalized, color = 'b')
+    plt.hist(img2
+             .flatten(),256,[0,256], color = 'r')
+    plt.xlim([0,256])
+    plt.legend(('cdf','histogram'), loc = 'upper left')
+    plt.show()
+
+    img2 = binarization(img2)
+    img3 = img2
+    # img3 = findObject(img1, img2)
+    #
+    name = binarization(getName(img3))
+    subName = binarization(getSubName(img3))
+    #
+    imgUp = connectVerticaly(getFace(img3), binarization(getSign(img3)))
+    imgDown = connectVerticaly(name, subName)
+    #
+    print img2str(name).upper()
+    print img2str(subName).upper()
     cv2.imshow('ID', connectVerticaly(imgUp,imgDown))
 
+    cv2.imshow('xD', img2)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
 
 main()
 print 'done'
